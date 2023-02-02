d808bf68aaec77a3e02a180671f53ba8bfe501aa
MinChan Lee <rlcks01537@gmail.com>
2023-02-02 17:05:17 +0900

    2023-02-02 js-clone-coding commit

--- js/greeting.js
+++ js/greeting.js
@@ -1,13 +1,13 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const loginInput = document.querySelector("#login-form Input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASS_NAME = "hidden";
+const Hidden_Class = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASS_NAME);
+  loginForm.classList.add(Hidden_Class);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
@@ -15,13 +15,13 @@ function onLoginSubmit(event) {
 
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASS_NAME);
+  greeting.classList.remove(Hidden_Class);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASS_NAME);
+  loginForm.classList.remove(Hidden_Class);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
